10844e412f5023a465a9bd7b08077224448cc660
Jonathan Malko <jon.malko@gmail.com>
2022-03-09 11:27:20 -0500

    added configs and output controls.  not ready for release.

--- src/Fieldtypes/Wistia.php
+++ src/Fieldtypes/Wistia.php
@@ -12,8 +12,48 @@ class Wistia extends \Statamic\Fields\Fieldtype
         ];
     }
 
+    public function configFieldItems(): array
+    {
+        return [
+            'embed_mode' => [
+                'display' => 'Mode',
+                'instructions' => 'Choose which mode you want to use',
+                'type' => 'select',
+                'default' => 'regular',
+                'options' => [
+                    'regular' => __('Regular Embed'),
+                    'popup' => __('Popup Embed'),
+                ],
+                'width' => 50
+            ],
+            'show_duration' => [
+                'display' => 'Display duration',
+                'instructions' => 'Show how long the video is.',
+                'type' => 'toggle',
+                'default' => false,
+                'width' => 50
+            ],
+            'track_watch' => [
+                'display' => 'Track video watch completion',
+                'instructions' => 'Mark videos as watched when they reach 90% watch.',
+                'type' => 'toggle',
+                'default' => false,
+                'width' => 50
+            ],
+            'track_finish' => [
+                'display' => 'Track video finish',
+                'instructions' => 'Mark videos as watched when they reach the end.',
+                'type' => 'toggle',
+                'default' => false,
+                'width' => 50
+            ],
+        ];
+    }
+
     public function augment($value) {
+        ray($this->config('embed_mode'));
+        // ray(get_class_methods($this));
         $wistia_id = basename($value);
-        return '<script src="https://fast.wistia.com/embed/medias/'.$wistia_id.'.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_'.$wistia_id.' videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/'.$wistia_id.'/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>'; 
+        return view('wistia::embed', compact(['wistia_id']));
     }
 }
