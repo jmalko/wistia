7d3069958aa90b19ac6ed0628cabe3fe62ee1ccf
Jonathan Malko <jon.malko@gmail.com>
2022-03-09 15:08:54 -0500

    added embed options - mode, title, duration, completion, finish

--- src/Fieldtypes/Wistia.php
+++ src/Fieldtypes/Wistia.php
@@ -19,41 +19,65 @@ class Wistia extends \Statamic\Fields\Fieldtype
                 'display' => 'Mode',
                 'instructions' => 'Choose which mode you want to use',
                 'type' => 'select',
-                'default' => 'regular',
+                'default' => 'inline',
                 'options' => [
-                    'regular' => __('Regular Embed'),
+                    'inline' => __('Inline Embed'),
                     'popup' => __('Popup Embed'),
                 ],
-                'width' => 50
+                'width' => 25
+            ],
+            'show_title' => [
+                'display' => 'Display title',
+                'instructions' => 'Show video title',
+                'type' => 'toggle',
+                'default' => false,
+                'width' => 25
             ],
             'show_duration' => [
                 'display' => 'Display duration',
                 'instructions' => 'Show how long the video is.',
                 'type' => 'toggle',
                 'default' => false,
-                'width' => 50
+                'width' => 25
             ],
-            'track_watch' => [
+            'track_completion' => [
                 'display' => 'Track video watch completion',
-                'instructions' => 'Mark videos as watched when they reach 90% watch.',
+                'instructions' => 'Mark videos as complete when they reach 90% unique seconds watched.',
                 'type' => 'toggle',
                 'default' => false,
-                'width' => 50
+                'width' => 25
             ],
             'track_finish' => [
                 'display' => 'Track video finish',
                 'instructions' => 'Mark videos as watched when they reach the end.',
                 'type' => 'toggle',
                 'default' => false,
-                'width' => 50
+                'width' => 25
             ],
         ];
     }
 
     public function augment($value) {
-        ray($this->config('embed_mode'));
-        // ray(get_class_methods($this));
+
+        $embed_mode = $this->config('embed_mode');
+        $show_duration = $this->config('show_duration');
+        $show_title = $this->config('show_title');
+        $track_completion = $this->config('track_completion');
+        $track_finish = $this->config('track_finish');
         $wistia_id = basename($value);
-        return view('wistia::embed', compact(['wistia_id']));
+
+        if (!$wistia_id) {
+            return;
+        }
+
+        return view('wistia::embed', compact([
+            'embed_mode',
+            'show_duration',
+            'show_title',
+            'track_completion',
+            'track_finish',
+            'wistia_id',
+        ]));
+
     }
 }
